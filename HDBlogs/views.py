8f5d75d779038fb451649d472c0e10d4d77514cc
HarshDubey <hdndcomputers6446@gmail.com>
2026-01-29 20:02:10 +0530

    Added @login_required to the /dashboard and changed login button to redirect to /dashboard instead of /login

--- HDBlogs/views.py
+++ HDBlogs/views.py
@@ -63,7 +63,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
         else:
             context={
                 'form' : form,
